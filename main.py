666e55db2532feb8b6ad85efbc09995e9118738c
p_kozhevnikov <p.kozhevnikov@cian.ru>
2024-10-26 11:38:57 +0300

    Added cors

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from contextlib import asynccontextmanager
-
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 
 from src.presentation.handlers.auth_handlers import user_router
@@ -16,6 +16,14 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all HTTP methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 app.include_router(user_router)
 app.include_router(game_router)
